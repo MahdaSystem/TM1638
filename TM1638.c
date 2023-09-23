d8f107f7c8b293bef56f14817bf4171bc1915dc9
Vinzenz82 <vinzenz_82@yahoo.de>
2023-09-23 20:18:23 +0200

    made the code nicer

--- TM1638.c
+++ TM1638.c
@@ -64,7 +64,7 @@
 /**
  * @brief  Convert HEX number to Seven-Segment code
  */
-const uint8_t HexTo7Seg[16+24] =
+const uint8_t HexTo7Seg[40] =
 {
   0x3F, // 0
   0x06, // 1
@@ -106,7 +106,6 @@ const uint8_t HexTo7Seg[16+24] =
   0x08, // _
   0x40, // -
   0x01  // high bar
-
 };
 
 
@@ -548,6 +547,25 @@ TM1638_SetMultipleDigit_HEX(TM1638_Handler_t *Handler, const uint8_t *DigitData,
                                  (const uint8_t *)DigitDataHEX, StartAddr, Count);
 }
 
+/**
+ * @brief  Set data to multiple digits in char format
+ * @param  Handler: Pointer to handler
+ * @param  DigitData: Array to Digits data. 
+ *                    Supported chars 0,1,2,3,4,5,6,7,8,9
+ *                                    A,b,C,d,E,F,g,G,h,H,i,I,j,l,L,n,N,o,O,P,q,r,S,t,u,U,y
+ *                                    _,-,high bar (use ~ to set)
+ * 
+ * @param  StartAddr: First digit position
+ *         - 0: Seg1
+ *         - 1: Seg2
+ *         - .
+ *         - .
+ *         - .
+ * 
+ * @param  Count: Number of segments to write data
+ * @retval TM1638_Result_t
+ *         - TM1638_OK: Operation was successful
+ */
 TM1638_Result_t
 TM1638_SetMultipleDigit_CHAR(TM1638_Handler_t *Handler, const uint8_t *DigitData,
                             uint8_t StartAddr, uint8_t Count)
@@ -700,8 +718,6 @@ TM1638_SetMultipleDigit_CHAR(TM1638_Handler_t *Handler, const uint8_t *DigitData
       case '~':
         DigitDataHEX[i] = HexTo7Seg[0x27] | DecimalPoint;
       break;
-
-
 
       default:
         DigitDataHEX[i] = 0;
